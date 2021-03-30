65e38805d35628d52730edd90d7c17975d0e2340
jpaivarinta <jouka.paivarinta@gmail.com>
2021-03-30 19:05:51 +0300

    account resource started

--- cryptomonitor/resources/account.py
+++ cryptomonitor/resources/account.py
@@ -1,7 +1,42 @@
-from flask_restful import Resource
-from flask_restful import Api
-from ..utils import CryptoMonitorBuilder
-from ..utils import create_error_response
+from flask_restful import Resource, Api
+from flask import request, Response
+from ..cryptodb import UserAccount
+from ..utils import CryptoMonitorBuilder, create_error_response
+
+
+class Accounts(Resource):
+    def get(self):
+        
+        if request.method != 'GET':
+            return "GET method required", 405
+
+        body = CryptoMonitorBuilder(items = [])
+        for single_account in UserAccount.query.all():
+            item =  CryptoMonitorBuilder(
+                id=single_account.id,
+                name=single_account.name,
+                portfolio_id=single_account.portfolio_id
+            )
+
+        """
+            Add control stuff here
+        """
+
+
+
+        """
+            Returning list of all useraccounts.
+        """
+        return Response(
+            status=200,
+            response=json.dumps(body),
+            mimetype=MASON
+        )
+
+
+    def post(self):
+        if not request.json:
+            return create_error_response(415, "Unsupported media type", "Request must be JSON")
 
 class Account(Resource):
     def get(self, name):
@@ -11,8 +46,3 @@ class Account(Resource):
     def delete(self,name):
         pass
 
-class Accounts(Resource):
-    def get(self):
-        pass
-    def post(self):
-        pass
