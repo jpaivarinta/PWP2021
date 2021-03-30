5c85e0f4813cd3e8916e2ead209f55e6441577f8
oulu_hillbilly <svaranka@gmail.com>
2021-03-30 19:11:19 +0300

    Rename app -> api

--- cryptomonitor/api.py
+++ cryptomonitor/api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask_restful import Api
 import json
 from flask_sqlalchemy import SQLAlchemy
 from database.cryptodb import db
@@ -8,6 +9,7 @@ from database.cryptodb import db
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database//cpdata.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+api = Api(app)
 
 # db.create_all()
 @app.route("/")
