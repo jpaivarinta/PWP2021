e98f4ab462527413fd6cc812ad7a5845539ec15e
Jonne Taipale <jonne.taipale@gmail.com>
2021-04-11 15:13:08 +0300

    api_tests

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -46,8 +46,79 @@ def _populate_db():
         db.session.add(p)
     db.session.commit()
 
+def _get_pcurrency_json(name, amount):
+    return {"currencyname": "{}".format(name), "currencyamount": "{}".format(amount)}
+
+def _get_account_json(name, password):
+    return {"name": "{}".format(name), "password": "{}".format(password)}
+
 def check_namespace(client, response):
     ns_href = response["@namespaces"]["crymo"]["name"]
     resp = client.get(ns_href)
     assert resp.status_code == 200
 
+def _check_control_get_method(ctrl, client, obj):
+    href = obj["@controls"][ctrl]["href"]
+    resp = client.get(href)
+    assert resp.status_code == 200
+
+def _check_control_delete_method(ctrl, client, obj):
+    href = obj["@controls"][ctrl]["href"]
+    method = obj["@controls"][ctrl]["method"].lower()
+    assert method == "delete"
+    resp = client.delete(href)
+    assert resp.status_code == 204
+
+def _check_control_put_method_pcurrency(ctrl, client, obj):
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "put"
+    assert encoding == "json"
+    body = _get_pcurrency_json("LTC", "200.0")
+    body["currencyname"] = obj["currencyname"]
+    validate(body, schema)
+    resp = client.put(href, json=body)
+    assert resp.status_code == 204
+
+def _check_control_put_method_account(ctrl, client, obj):
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "put"
+    assert encoding == "json"
+    body = _get_account_json("pekka", "testpwd")
+    body["name"] = obj["name"]
+    validate(body, schema)
+    resp = client.put(href, json=body)
+    assert resp.status_code == 204
+
+def _check_control_post_method_pcurrency(ctrl, client, obj):
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "post"
+    assert encoding == "json"
+    body = _get_pcurrency_json("LTC", "200.0")
+    validate(body, schema)
+    resp = client.post(href, json=body)
+    assert resp.status_code == 201
+
+def _check_control_post_method_account(ctrl, client, obj):
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "post"
+    assert encoding == "json"
+    body = _get_account_json("pekka", "testpwd")
+    validate(body, schema)
+    resp = client.post(href, json=body)
+    assert resp.status_code == 201
